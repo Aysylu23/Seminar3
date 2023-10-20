075742c6b1f991330b231ff2b6a80d1cd5dedff3
Aysylu23 <aysbad25@gmail.com>
2023-10-20 20:05:49 +0500

    Add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,67 @@
+﻿/*Задача 17*/
+/*Console.Write("Введите координату X: ");
+string inputX = Console.ReadLine();
+int x = int.Parse(inputX);
+
+Console.Write("Введите координату Y: ");
+string inputY = Console.ReadLine();
+int y = int.Parse(inputY);
+
+if(x == 0 || y == 0)
+{
+    Console.WriteLine("X и Y не могут быть равны нулю");
+}
+else
+{
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("Четверть 1");
+    }
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("Четверть 2");
+    }
+    else if (x < 0 && y < 0)
+    {
+        Console.WriteLine("Четверть 3");
+    }
+    else if (x > 0 && y < 0)
+    {
+        Console.WriteLine("Четверть 4");
+    }
+}*/
+
+
+
+int x = ReadInt("Введите координату X: ");
+int y = ReadInt("Введите координату Y: ");
+
+if(x == 0 || y == 0)
+{
+    Console.WriteLine("X и Y не могут быть равны нулю");
+    return;
+}
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Четверть 1");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("Четверть 2");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Четверть 3");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("Четверть 4");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    string input = Console.ReadLine();
+    return int.Parse(input);
+}
