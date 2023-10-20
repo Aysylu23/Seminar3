f04343b3fcc223786df9497f3e7e10dfb69c1c98
Aysylu23 <aysbad25@gmail.com>
2023-10-20 21:37:23 +0500

    Add homework23

---
+++ Homework23/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, котрая на вход принимает на вход число(N) и выдает таблицу кубов чисел от 1 до N
+3 -> 1, 4, 9.
+5 -> 1, 8, 27, 64, 125.*/
+
+int number = ReadInt("Введите чило: ");
+int i = 1;
+
+while(i <= number)
+{
+    int sqr = i * i * i;
+
+    if(i == number)
+    {
+        Console.Write(sqr + ". ");
+        break;
+    }
+
+    Console.Write(sqr + ", ");
+    i += 1;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    string input = Console.ReadLine();
+    return int.Parse(input);
+}
