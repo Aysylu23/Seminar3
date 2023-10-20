ba0545b7a7768f5d062518138662f9377dcda0bc
Aysylu23 <aysbad25@gmail.com>
2023-10-20 20:22:56 +0500

    Add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+от 1 до N.
+
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4*/
+
+int number = ReadInt("Введите чило: ");
+int i = 1;
+
+while(i <= number)
+{
+    int sqr = i * i;
+
+    if(i == number)
+    {
+        Console.Write(sqr + ". ");
+        break;
+    }
+
+    Console.Write(sqr + ", ");
+    i += 1;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    string input = Console.ReadLine();
+    return int.Parse(input);
+}
