9e08be1ee09cf671648136c6679a56a57c466080
Aysylu23 <aysbad25@gmail.com>
2023-10-21 23:24:12 +0500

    Add homework19

---
+++ Homework19/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+14212 -> нет
+18564 -> нет
+21212 -> да*/
+
+int number = ReadInt("Введите чило: ");
+
+
+
+if (number < 10000 || number > 99999)
+{ 
+    Console.WriteLine("Введите пятизначное число");
+    return;
+}
+else if (number / 10000 == number % 10) 
+{
+    Console.WriteLine("да");
+}
+
+else if (number / 1000 % 10 == number % 100 / 10)
+{
+    Console.WriteLine("да");
+}
+
+else 
+{
+    Console.WriteLine("нет");
+}
+
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    string input = Console.ReadLine();
+    return int.Parse(input);
+}
+
